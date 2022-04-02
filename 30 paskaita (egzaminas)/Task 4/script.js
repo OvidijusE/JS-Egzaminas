94a2853fb8668dc461cc26f0ae1be5832f4bd775
Ovidijus <ovidijus.e@gmail.com>
2022-04-02 10:50:44 +0300

    task 4 in progress

--- 30 paskaita (egzaminas)/Task 4/script.js
+++ 30 paskaita (egzaminas)/Task 4/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* ------------------------------ TASK 4 -----------------------------------
 Parašykite JS kodą, vartotjui atėjus į tinkaį kreipsis į cars.json failą
 ir iš atvaizduos visus automobilių gamintojus ir pagamintus modelius. 
@@ -9,4 +11,34 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+
+const outputEl = document.getElementById("output");
+
+async function getCars() {
+  const resp = await fetch(ENDPOINT);
+  const data = await resp.json();
+  renderCars(data, outputEl);
+}
+getCars();
+
+function renderCars(carsArr, dest) {
+  dest.innerHTML = "";
+  carsArr.forEach((cObj) => {
+    dest.append(showCars(cObj));
+  });
+}
+
+function showCars(carObj) {
+  const divEl = document.createElement("div");
+  divEl.className = "cars-card";
+  divEl.innerHTML = `
+  
+  
+  <h3 class="car-brand">${carObj.brand}</h3>
+  <p class="car-models">${carObj.models.join(" ")}</p>
+  
+      `;
+
+  return divEl;
+}
