05fe538ffad854be00eeb319d03cb9098fc678a3
Ovidijus <ovidijus.e@gmail.com>
2022-04-02 15:16:18 +0300

    task 9 done

--- 30 paskaita (egzaminas)/Task 9/script.js
+++ 30 paskaita (egzaminas)/Task 9/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /* ------------------------------ TASK 9 ---------------------------------------------------------------
 Sukurkite konstruktoriaus funkciją "Movie" (naudokte ES6), kuri sukuria objektus 3 savybėm ir 1 metodu:
 
@@ -6,3 +8,21 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+    this.wasExpensive = function () {
+      if (this.budget > 100000000) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+  }
+}
+
+const newMovie = new Movie("Zero", "Emilis Velyvis", 100000001);
+// console.log(newMovie.wasExpensive());
