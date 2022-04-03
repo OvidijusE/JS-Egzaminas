010f9364c1743e25af089fa357067deab1d6a97c
Ovidijus <ovidijus.e@gmail.com>
2022-04-03 18:03:17 +0300

    task 10 done

--- 30 paskaita (egzaminas)/Task 8/script.js
+++ 30 paskaita (egzaminas)/Task 8/script.js
@@ -8,29 +8,33 @@ multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
 
-function Calculator(num1, num2) {
-  this.num1 = num1;
-  this.num2 = num2;
-
-  this.sum = function () {
+function Calculator() {
+  this.sum = function (num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
     return this.num1 + this.num2;
   };
 
-  this.subtraction = function () {
+  this.subtraction = function (num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
     return this.num1 - this.num2;
   };
-  this.multiplication = function () {
+  this.multiplication = function (num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
     return this.num1 * this.num2;
   };
-  this.division = function () {
+  this.division = function (num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
     return this.num1 / this.num2;
   };
 }
 
 let calculator = new Calculator();
-// calculator.read();
 
-console.log(calculator.sum(1, 5));
-console.log(calculator.subtraction(10, 2));
-console.log(calculator.multiplication(5, 3));
-console.log(calculator.division(3, 2));
+console.log(calculator.sum(1, 8));
+console.log(calculator.subtraction(10, 8));
+console.log(calculator.multiplication(5, 12));
+console.log(calculator.division(5, 2));
